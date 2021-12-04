66132faba3c61e333dcef61bf9931e0eb2223681
Afaed <44733498+Afaed@users.noreply.github.com>
2021-12-03 20:48:20 -0800

    Managed to have the columns displayed

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,16 @@
+var date = document.getElementById('currentDay')
+
+var currentDate = new Date ();
+
+$('div').click(function(){
+  var prev=$(this).html();
+  $(this).html($(this).data('prev'));
+  $(this).data('prev',prev)
+  console.log($(this).data('prev'))
+  })
+$("#modalDueDate").datapicker();
+
+
 $(".card .list-group").sortable({
     connectWith: $(".card .list-group"),
     scroll: false,
@@ -18,3 +31,13 @@ $(".card .list-group").sortable({
     update: function(event) {
       console.log("update", this);
     }});
+
+    $(".slot").click({
+  connectWith: $(".slot"),
+  scroll: false,
+  tolerance: "pointer",
+  helper:"clone",
+  activate: function(event) {
+    console.log("activate", this)
+  }
+})
