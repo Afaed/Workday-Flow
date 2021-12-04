8fc9b29f7e11220c92a20bb10b9844d1438a4659
Afaed <44733498+Afaed@users.noreply.github.com>
2021-12-03 21:56:22 -0800

    Managed to have the columns displayed

--- Develop/script.js
+++ Develop/script.js
@@ -1,43 +1,57 @@
-var date = document.getElementById('currentDay')
+var date = document.getElementById("currentDay");
+var timeContainer = document.getElementsByClassName("timeblocks");
+var currentDate = new Date();
 
-var currentDate = new Date ();
+$("div").click(function () {
+  var prev = $(this).html();
+  $(this).html($(this).data("prev"));
+  $(this).data("prev", prev);
+  console.log($(this).data("prev"));
+for(i = 0; i<=div.length; i++)
 
-$('div').click(function(){
-  var prev=$(this).html();
-  $(this).html($(this).data('prev'));
-  $(this).data('prev',prev)
-  console.log($(this).data('prev'))
-  })
-$("#modalDueDate").datapicker();
-
+;});
 
 $(".card .list-group").sortable({
-    connectWith: $(".card .list-group"),
-    scroll: false,
-    tolerance: "pointer",
-    helper: "clone",
-    activate: function(event) {
-      console.log("activate", this);
-    },
-    deactivate: function(event) {
-      console.log("deactivate", this);
-    },
-    over: function(event) {
-      console.log("over", event.target);
-    },
-    out: function(event) {
-      console.log("out", event.target);
-    },
-    update: function(event) {
-      console.log("update", this);
-    }});
-
-    $(".slot").click({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function (event) {
+    console.log("activate", this);
+  },
+  deactivate: function (event) {
+    console.log("deactivate", this);
+  },
+  over: function (event) {
+    console.log("over", event.target);
+  },
+  out: function (event) {
+    console.log("out", event.target);
+  },
+  update: function (event) {
+    console.log("update", this);
+  }
+});
+for (let i = 0; i < 10; i++){
+$(".slot").click({
   connectWith: $(".slot"),
   scroll: false,
   tolerance: "pointer",
-  helper:"clone",
-  activate: function(event) {
-    console.log("activate", this)
+  helper: "clone",
+  activate: function (event) {
+    console.log("activate", this);
   }
-})
+  }
+);};
+
+for (let i = 0; i < 20; i++){
+$(".timeBlocks")(`   <div class="col-2 border border-dark">
+<p>1/3 Timeblocks here</p>
+</div>
+<div class="col-8 border border-primary">
+<input placeholder="2 of 3 - type here" ></input>
+</div>
+<div class="col-2 border border-success">
+<p>3/3 save button here</p>
+</div>
+</div>`)};
