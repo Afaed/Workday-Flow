7f1440ec78afdef22a7b9ad39a10910ae731b6cb
Afaed <44733498+Afaed@users.noreply.github.com>
2021-12-04 22:40:28 -0800

    New commit

--- Develop/script.js
+++ Develop/script.js
@@ -1,71 +1,56 @@
-var timeContainer = document.getElementsByClassName("timeblocks");
-
-var today = document.querySelector("#currentDay");
-var currentTime = moment();
-today.textContent = currentTime.format("dd, MMM YYYY");
-
-$("div").click(function () {
-  var prev = $(this).html();
-  $(this).html($(this).data("prev"));
-  $(this).data("prev", prev);
-  console.log($(this).data("prev"));
-for(i = 0; i<=div.length; i++)
-
-;});
-
-console.log(tempArr);
-$(".card .list-group").sortable({
-  connectWith: $(".card .list-group"),
-  scroll: false,
-  tolerance: "pointer",
-  helper: "clone",
-  activate: function (event) {
-    console.log("activate", this);
-  },
-  deactivate: function (event) {
-    console.log("deactivate", this);
-  },
-  over: function (event) {
-    console.log("over", event.target);
-  },
-  out: function (event) {
-    console.log("out", event.target);
-  },
-  update: function (event) {
-    console.log("update", this);
-  }
-});
-let columnsNames = ["col1", "col2", "col3", "col4"];
-let rows = [
-["col1val1", "09", "26", "1"],
-["col1val2", "08", "59", "1"],
-["col1val3", "09", "22", "1"]
-];
+//using moment to get the current time
+currentTime = moment();
+//connected it to the current day spot in the html
+currentDayEl = document.querySelector("#currentDay");
+var displayDate = currentTime.format('MMMM Do YYYY');
+//backticks are a way of adding dynamic things into a string when used with a dollar sign
 
 
-var _rows = [];
-var rlen = rows.length;
-var clen = columnsNames.length;
-for(var i=0;i<rlen;i++){
-    var _row = {};
-    for(var c=0;c<clen;c++){
-        _row[columnsNames[c]] = rows[i][c];
+currentDayEl.innerText = displayDate
+
+var timeBlockEl = document.querySelectorAll(".time-block");
+
+var checkTime = function (currentEl) {
+    var timeBlock = moment().hour(parseInt(currentEl.id))
+    console.log(timeBlock)
+    
+    if (timeBlock.hour() < currentTime.hour()) {
+        return "past"
     }
-    _rows.push(_row);
+    if (timeBlock.hour() > currentTime.hour()) {
+        return "future"
+    }
+    if (timeBlock.hour() === currentTime.hour()) {
+        return "present"
+    }
+
+for (i = 0; i <= timeBlockEl.length - 1; i++) {
+    timeBlockEl[i].className = timeBlockEl[i].className + " " + checkTime(timeBlockEl[i])
+}}
+
+
+var onSave = function (event) {
+
+    if (document.querySelector(`#btn${event.target.value}`).value) {
+        
+        var timeBlockEvent = document.querySelector(`#btn${event.target.value}`).value
+
+        localStorage.setItem(`${event.target.value}`, timeBlockEvent.trim())
+    }
+   
 }
 
-console.log(_rows);
+$(".saveBtn").click(onSave)
 
-var col = document.createElement("div");
-const col= getElementById(".col")
-for (let i = 0; i < 20; i++){
-$(".timeBlocks")(`   <div class="col-2 border border-dark">
-<p>1/3 Timeblocks here</p>
-</div>
-<div class="col-8 border border-primary">
-<input placeholder="2 of 3 - type here" ></input>
-</div>
-<div class="col-2 border border-success">
-<p>3/3 save button here</p>
-</div>
-</div>`)};
+var eventArray = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
+
+
+
+for (var key of eventArray) {
+
+    var eventText = localStorage.getItem(`${key}`)
+    if (eventText) {
+        $(`#btn${key}`).val(eventText)
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Work Day Scheduler Starter Code
+
+
+Challenges
+
+It turns out that when I was studying hte modules, I had the right idea of how to do it and how it should work. I had the Moment() function down and all that stuff, but for some odd reason it did not work. A major obstacle is in teh following image. 
+
+I did get help from Maomi who shared me her code to see what I could do and couldn't do as weell as how to do the assignment
